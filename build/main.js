893f9c570fb4af2a7c8dd3076567f3e2949ce01c
velopert <public.velopert@gmail.com>
2016-07-01 06:47:51 +0900

    mongoose, express-session

--- build/main.js
+++ build/main.js
@@ -8,6 +8,10 @@ var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
 
+var _mongoose = require('mongoose');
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
 var _webpack = require('webpack');
 
 var _webpack2 = _interopRequireDefault(_webpack);
@@ -16,22 +20,58 @@ var _webpackDevServer = require('webpack-dev-server');
 
 var _webpackDevServer2 = _interopRequireDefault(_webpackDevServer);
 
+var _morgan = require('morgan');
+
+var _morgan2 = _interopRequireDefault(_morgan);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _expressSession = require('express-session');
+
+var _expressSession2 = _interopRequireDefault(_expressSession);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var app = (0, _express2.default)();
+// HTTP REQUEST LOGGER
+
+
+var app = (0, _express2.default)(); // PARSE HTML BODY
+
+
+/* import express middlewares */
+
 var port = 3000;
 var devPort = 4000;
 
+/* mongodb connection */
+var db = _mongoose2.default.connection;
+db.on('error', console.error);
+db.once('open', function () {
+    console.log('Connected to mongodb server');
+});
+// mongoose.connect('mongodb://username:password@host:port/database=');
+_mongoose2.default.connect('mongodb://localhost/codelab');
+
+/* setup express middlewares */
+app.use((0, _morgan2.default)('dev'));
+app.use(_bodyParser2.default.json());
+app.use((0, _expressSession2.default)({
+    secret: 'CodeLab1$1$234',
+    resave: false,
+    saveUninitialized: true
+}));
+
+/* setup routers & static directory */
 app.use('/', _express2.default.static(_path2.default.join(__dirname, './../public')));
 
-app.get('/hello', function (req, res) {
-    return res.send('Hello CodeLab');
-});
-
+/* listen server */
 app.listen(port, function () {
     console.log('Express is listening on port', port);
 });
 
+/* listen devserver */
 if (process.env.NODE_ENV == 'development') {
     console.log('Server is running on development mode');
     var config = require('../webpack.dev.config');
--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
 
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
@@ -7,14 +8,30 @@ import WebpackDevServer from 'webpack-dev-server';
 /* import express middlewares */
 import morgan from 'morgan'; // HTTP REQUEST LOGGER
 import bodyParser from 'body-parser'; // PARSE HTML BODY
+import session from 'express-session';
 
 const app = express();
 const port = 3000;
 const devPort = 4000;
 
+/* mongodb connection */
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => { console.log('Connected to mongodb server'); });
+// mongoose.connect('mongodb://username:password@host:port/database=');
+mongoose.connect('mongodb://localhost/codelab');
+
+
+
 /* setup express middlewares */
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'CodeLab1$1$234',
+    resave: false,
+    saveUninitialized: true
+}));
+
 
 /* setup routers & static directory */
 app.use('/', express.static(path.join(__dirname, './../public')));
