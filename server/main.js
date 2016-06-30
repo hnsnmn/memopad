372080ecccb59f09e98fa2eb197943f548cf6ce4
velopert <public.velopert@gmail.com>
2016-07-01 06:28:33 +0900

    .

--- server/main.js
+++ server/main.js
@@ -4,6 +4,7 @@ import path from 'path';
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 
+/* import express middlewares */
 import morgan from 'morgan'; // HTTP REQUEST LOGGER
 import bodyParser from 'body-parser'; // PARSE HTML BODY
 
@@ -11,19 +12,19 @@ const app = express();
 const port = 3000;
 const devPort = 4000;
 
+/* setup express middlewares */
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+/* setup routers & static directory */
 app.use('/', express.static(path.join(__dirname, './../public')));
 
-app.get('/hello', (req, res) => {
-    return res.send('Hello CodeLab');
-});
-
+/* listen server */
 app.listen(port, () => {
     console.log('Express is listening on port', port);
 });
 
+/* listen devserver */
 if(process.env.NODE_ENV == 'development') {
     console.log('Server is running on development mode');
     const config = require('../webpack.dev.config');
