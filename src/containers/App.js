538ed4c616d1b927c9962ba20943a42c275f9cf5
velopert <public.velopert@gmail.com>
2016-07-03 01:47:25 +0900

    hide header when login/register, no need react-redux-router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import { Header } from 'components';
 
 class App extends React.Component {
     render(){
+        /* Check whether current route is login or register using regex */
+        let re = /^(login|register)$/;
+        let isAuth = re.test(this.props.location.pathname);
+
         return (
             <div>
-                <Header/>
+                { !isAuth ? (<Header/>) : (<div/>) }
                 { this.props.children }
             </div>
         );
     }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,25 +3,16 @@ import ReactDOM from 'react-dom';
 
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 import { App, Login, Register, Home } from 'containers';
-
-import { push, routerMiddleware, syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reducers from 'reducers';
-
-
-
-const middleware = routerMiddleware(browserHistory);
-const store = createStore(reducers, applyMiddleware(middleware));
-const history = syncHistoryWithStore(browserHistory, store);
-
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-    <Router history={browserHistory}>
-        <Route path="/" component={App}>
-            <IndexRoute component={Home}/>
-            <Route path="home" component={Home}/>
-            <Route path="login" component={Login}/>
-            <Route path="register" component={Register}/>
-        </Route>
-    </Router>, rootElement);
+        <Router history={browserHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={Home}/>
+                <Route path="home" component={Home}/>
+                <Route path="login" component={Login}/>
+                <Route path="register" component={Register}/>
+            </Route>
+        </Router>, rootElement);
