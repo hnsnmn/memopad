91354fa3bf501440fe7fc35a0273e1e813ede0ce
velopert <public.velopert@gmail.com>
2016-07-03 01:55:53 +0900

    regex fix, increate margin

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,8 +6,9 @@ import { Header } from 'components';
 class App extends React.Component {
     render(){
         /* Check whether current route is login or register using regex */
-        let re = /^(login|register)$/;
+        let re = /(login|register)/;
         let isAuth = re.test(this.props.location.pathname);
+        console.log(this.props.location.pathname);
 
         return (
             <div>
