69898df688da09217974d150c03357d5fbbe1b63
velopert <public.velopert@gmail.com>
2016-07-03 03:03:09 +0900

    Reinstall fetch.. (installd wrong one)

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,5 +1,7 @@
-export const AUTH_LOGIN_REQ = "AUTH_LOGIN_REQ";
+/* AUTHENTICATION */
+
+export const AUTH_LOGIN = "AUTH_LOGIN";
 export const AUTH_LOGIN_RCV = "AUTH_LOGIN_RECV";
 
-export const AUTH_REGISTER_REQ = "AUTH_REGISTER_REQ";
+export const AUTH_REGISTER = "AUTH_REGISTER";
 export const AUTH_REGISTER_RCV = "AUTH_REGISTER_RCV";
---
+++ src/actions/authentication.js
@@ -0,0 +1,25 @@
+import fetch from 'whatwg-fetch';
+
+import { AUTH_LOGIN,
+    AUTH_LOGIN_RCV,
+    AUTH_REGISTER,
+    AUTH_REGISTER_RCV } from './ActionTypes';
+
+
+export function login(username, password) {
+    return (dispatch) => {
+
+    };
+}
+
+export function loginReceived(response) {
+
+}
+
+export function register() {
+
+}
+
+export function registerReceived(response) {
+
+}
