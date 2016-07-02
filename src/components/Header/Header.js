b6023e4e0eb1cc6a1b2062d191ad87ca695f79d3
velopert <public.velopert@gmail.com>
2016-07-02 22:20:04 +0900

    style Header Component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 class Header extends React.Component {
     render() {
-        return (<div>Header</div>);
+        return (
+            <nav>
+                <div className="nav-wrapper blue darken-1">
+                    <a href="#" className="brand-logo center">THEMEMO</a>
+
+                    <ul>
+                        <li><a href="#"><i className="material-icons">search</i></a></li>
+                    </ul>
+
+                    <div className="right">
+                        <ul>
+                            <li><a href="#"><i className="material-icons">vpn_key</i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
     }
 }
 
