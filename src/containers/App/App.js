7ac6846972dc65f8cbd846dbc1581f8e54e97637
velopert <public.velopert@gmail.com>
2016-07-02 22:05:51 +0900

    Create Header

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { Header } from 'components';
+
 class App extends React.Component {
     render(){
-
         return (
+            <div>
+                <Header/>
                 <h1>Hello React Skeleton!!</h1>
+            </div>
         );
     }
 }
