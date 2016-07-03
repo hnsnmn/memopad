8aa083cca12d6b1ae60d97f0f3c2f0902db17f41
velopert <public.velopert@gmail.com>
2016-07-04 05:48:21 +0900

    Write basic elements

--- src/components/Write.js
+++ src/components/Write.js
@@ -3,7 +3,16 @@ import React from 'react';
 class Write extends React.Component {
     render() {
         return (
-            <div className="container">WRITE</div>
+            <div className="container">
+                <div className="card">
+                    <div className="card-content">
+                        <textarea className="materialize-textarea"></textarea>
+                    </div>
+                    <div className="card-action">
+                      <a href="#" className="">POST</a>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
