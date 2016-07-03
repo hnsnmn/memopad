81ea1eec07f743c68b279ea39a50821ae143665f
velopert <public.velopert@gmail.com>
2016-07-04 06:46:18 +0900

    Write component setup

--- src/components/Write.js
+++ src/components/Write.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
 class Write extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            contents: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            contents: e.target.value
+        });
+    }
+
     render() {
         return (
             <div className="container write">
                 <div className="card">
                     <div className="card-content">
-                        <textarea className="materialize-textarea"></textarea>
+                        <textarea className="materialize-textarea"
+                            onChange={this.handleChange}
+                            value={this.state.contents}
+                            placeholder="Write down your memo"></textarea>
                     </div>
                     <div className="card-action">
                       <a href="#" className="">POST</a>
