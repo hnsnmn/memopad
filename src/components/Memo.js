6380c6fd1964565e43e480baf6e18a6a71f855ad
velopert <public.velopert@gmail.com>
2016-07-04 09:32:42 +0900

    Memo Component Basic Design

--- src/components/Memo.js
+++ src/components/Memo.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Dropdown, NavItem } from 'react-materialize';
+
 
 class Memo extends React.Component {
+
     render() {
         return(
             <div className="container memo">
-                <h1>MEMO</h1>
+                <div className="card">
+                    <div className="info">
+                        <span className="username">velopert</span> wrote a log · 2 hours ago
+                        <div className="right">
+                            <Dropdown trigger={<div><i className="material-icons icon-button">more_vert</i></div>}>
+                                <NavItem>Edit</NavItem>
+                                <NavItem>Remove</NavItem>
+                            </Dropdown>
+                        </div>
+                    </div>
+                    <div className="card-content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+                </div>
             </div>
         );
     }
